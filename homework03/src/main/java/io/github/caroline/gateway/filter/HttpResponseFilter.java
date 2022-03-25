8b7b90088920d28dc8c25385ebaf8369e944ad37
caroline.zhang <caroline.zhang@cloudwise.com>
2022-03-25 18:20:43 +0800

    规范代码格式

--- homework03/src/main/java/io/github/caroline/gateway/filter/HttpResponseFilter.java
+++
@@ -1,9 +0,0 @@
-package io.github.caroline.gateway.filter;
-
-import io.netty.handler.codec.http.FullHttpResponse;
-
-public interface HttpResponseFilter {
-
-    void filter(FullHttpResponse response);
-
-}
--- homework03/src/main/java/io/github/caroline/gateway/outbound/okhttp/OkhttpOutboundHandler.java
+++
@@ -1,4 +0,0 @@
-package io.github.caroline.gateway.outbound.okhttp;
-
-public class OkhttpOutboundHandler {
-}
